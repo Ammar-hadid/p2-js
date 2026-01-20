27bebab12e3e8b7f8a1f5cf402091c1ee5f045f1
Ammar-hadid <9017886@student.zadkine.nl>
2026-01-20 10:54:31 +0100

    Maak opdracht 25

---
+++ w6/opdracht_25/script.js
@@ -0,0 +1,19 @@
+const message = document.querySelector('h1');
+
+startLoop()
+
+function generateRandomHex() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
+}
+
+function renderColor() {
+    const hex = generateRandomHex();
+    message.textContent = hex;
+
+    document.body.style.setProperty('--random-color', hex);
+}
+
+function startLoop() {
+    setInterval(renderColor, 500);
+}
+
